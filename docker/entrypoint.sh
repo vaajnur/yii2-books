f985ed2b8158635048f3b5e407d3b36c42f9e364
ainur home <ainur home>
2026-08-15 23:14:55 +0300

    windows compitable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,10 @@ mkdir -p /app/runtime /app/web/assets /app/web/uploads/covers
 chown -R www-data:www-data /app/runtime /app/web/assets /app/web/uploads
 chmod -R ug+rwX /app/runtime /app/web/assets /app/web/uploads
 
+if [ ! -f /app/vendor/autoload.php ] || [ ! -f /app/vendor/yiisoft/yii2-gii/src/Module.php ]; then
+    composer install --working-dir=/app --no-interaction --prefer-dist
+fi
+
 php /app/yii migrate --interactive=0
 
 exec docker-php-entrypoint apache2-foreground
